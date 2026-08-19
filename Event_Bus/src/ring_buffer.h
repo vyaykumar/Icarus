2e9fb5157a2b686c1c2b92d0000cb599a8774efc
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-08-19 12:11:15 +0530

    [P1] fix: Added empty/full checks to the push() and pop().

--- Event_Bus/src/ring_buffer.h
+++ Event_Bus/src/ring_buffer.h
@@ -10,17 +10,27 @@ struct RingBuffer {
     std::atomic<uint32_t> write_index {0};
     std::atomic<uint32_t> read_index {0};
 
-    void push (const Event& event) {
-        uint32_t idx = write_index.load(std::memory_order_relaxed);
-        buffer[idx] = event;
-        write_index.store((idx+1) % Capacity, std::memory_order_release);
+    bool push (const Event& event) {
+        const uint32_t w_idx = write_index.load(std::memory_order_relaxed);
+        const uint32_t r_idx = read_index.load(std::memory_order_acquire);
+        const uint32_t next_w_idx = (w_idx+1) % Capacity;
+
+        if (next_w_idx == r_idx) return false;
+
+        buffer[w_idx] = event;
+        write_index.store((w_idx+1) % Capacity, std::memory_order_release);
+        return true;
     }
 
-    Event pop () {
-        uint32_t idx = read_index.load(std::memory_order_acquire);
-        const Event temp = buffer[idx];
-        read_index.store((idx+1) % Capacity, std::memory_order_release);
-        return temp;
+    bool pop () {
+        const uint32_t w_idx = write_index.load(std::memory_order_relaxed);
+        const uint32_t r_idx = read_index.load(std::memory_order_acquire);
+
+        if (r_idx == w_idx) return false;
+
+        const Event temp = buffer[r_idx];
+        read_index.store((r_idx+1) % Capacity, std::memory_order_release);
+        return true;
     }
 
 };
