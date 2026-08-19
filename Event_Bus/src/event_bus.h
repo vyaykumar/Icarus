5a59ab5aae6de2ebd87454961bf2beaccf346ffa
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-08-19 13:55:38 +0530

    [P1] feat: Fleshed out EventBus.

--- Event_Bus/src/event_bus.h
+++ Event_Bus/src/event_bus.h
@@ -1,8 +1,19 @@
-//
-// Created by VijayKumar on 19-08-2026.
-//
-
 #ifndef ICARUS_EVENT_BUS_H
 #define ICARUS_EVENT_BUS_H
+
+#include <cstdint>
+#include "ring_buffer.h"
+#include "memory_pool.h"
+
+template <uint32_t BufferCapacity, uint32_t PayloadCapacity>
+struct EventBus {
+    RingBuffer<BufferCapacity> command_buffer;
+    RingBuffer<BufferCapacity> response_buffer;
+    MemoryPool<uint8_t, PayloadCapacity> payload_pool;
+
+    uint32_t latency_ns;
+    uint32_t jitter_ns;
+    float packet_loss_rate;
+};
 
 #endif //ICARUS_EVENT_BUS_H
--- Event_Bus/src/memory_pool.h
+++ Event_Bus/src/memory_pool.h
@@ -1,8 +1,18 @@
-//
-// Created by VijayKumar on 19-08-2026.
-//
-
 #ifndef ICARUS_MEMORY_POOL_H
 #define ICARUS_MEMORY_POOL_H
+#include <array>
+#include <cstdint>
+
+template <typename T, uint32_t Capacity>
+struct MemoryPool {
+    struct Handle {
+        uint32_t index;
+        uint32_t generation;
+    };
+
+    std::array<T,Capacity> storage;
+    std::array<uint32_t,Capacity> generations;
+    std::array<bool, Capacity> available;
+};
 
 #endif //ICARUS_MEMORY_POOL_H
