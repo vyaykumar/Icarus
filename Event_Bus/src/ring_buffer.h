0811e030b23e6fd619a4a434b52593b90bcb95fc
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-08-19 11:57:47 +0530

    [P1] feat: Created RingBuffer struct. Implemented basic push and pop.

--- Event_Bus/src/ring_buffer.h
+++ Event_Bus/src/ring_buffer.h
@@ -1,5 +1,26 @@
 #ifndef ICARUS_RING_BUFFER_H
 #define ICARUS_RING_BUFFER_H
+#include <atomic>
+
+#include "event.h"
+
+template <uint32_t Capacity>
+struct RingBuffer {
+    Event buffer [Capacity];
+    std::atomic<uint32_t> write_index {0};
+    std::atomic<uint32_t> read_index {0};
+
+    void push (const Event& event) {
+        buffer[write_index++] = std::move(event);
+    }
+
+    Event pop () {
+        auto temp = buffer[read_index];
+        buffer[read_index++] = {};
+        return std::move(temp);
+    }
+
+};
 
 
 
