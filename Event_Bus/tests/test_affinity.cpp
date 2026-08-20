221201534b823b5501140f1cd4c48fc5af4bf0c2
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-08-20 22:11:49 +0530

    [P2] feat: Affinity's test placeholder.

--- Event_Bus/tests/test_affinity.cpp
+++ Event_Bus/tests/test_affinity.cpp
@@ -1,3 +1,21 @@
-//
-// Created by VijayKumar on 20-08-2026.
-//
+#include <gtest/gtest.h>
+#include <thread>
+#include "../src/core/affinity.h"
+
+using namespace icarus::core;
+
+TEST(AffinityTest, BindThreadToCore) {
+    // TODO: Create an int counter initialized to 0.
+    // TODO: Create a std::thread with a lambda that increments the counter 1000 times.
+    // TODO: Call bind_thread_to_core(thread, 1).
+    // TODO: Assert no exception is thrown.
+    // TODO: Join the thread.
+    // TODO: Assert counter == 1000.
+}
+
+TEST(AffinityTest, BindThreadInvalidCore) {
+    // TODO: Create a std::thread with a lambda that does nothing.
+    // TODO: Call bind_thread_to_core(thread, -1).
+    // TODO: Expect an std::runtime_error to be thrown.
+    // TODO: Join the thread.
+}
